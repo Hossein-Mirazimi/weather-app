f521d99860e987c34e4be10ff05b2bd1c1f18eed
Hossein Mirazimi <h.mirazimi@alibaba.ir>
2021-11-26 18:12:33 +0330

    chore: add prettier config and remove extra code

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,70 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-unused-expressions": 0,
+    'prettier/prettier': [
+      'error',
+      {
+        'endOfLine': 'auto',
+      }
+    ],
+    "vue/order-in-components": [
+      "warn",
+      {
+          "order": [
+              "el",
+              "name",
+              "key",
+              "parent",
+              "functional",
+              "watchQuery",
+              "layout",
+              "middleware",
+              "validate",
+              "head",
+              "fetch",
+              "asyncData",
+              [
+                  "delimiters",
+                  "comments"
+              ],
+              [
+                  "components",
+                  "directives",
+                  "filters"
+              ],
+              "extends",
+              "mixins",
+              [
+                  "provide",
+                  "inject"
+              ],
+              "ROUTER_GUARDS",
+              "scrollToTop",
+              "transition",
+              "loading",
+              "inheritAttrs",
+              "model",
+              [
+                  "props",
+                  "propsData"
+              ],
+              "emits",
+              "setup",
+              "data",
+              "computed",
+              "watch",
+              "LIFECYCLE_HOOKS",
+              "methods",
+              [
+                  "template",
+                  "render"
+              ],
+              "renderError"
+          ]
+      }
+    ]
   },
   overrides: [
     {
